b875eb1f839c7a2160e1e8ed4ae56b20d2b1bc5f
contactkislay <contactkislay@gmail.com>
2015-08-07 21:57:35 -0700

    Project 1: Minor changes in Plot 3

--- plot3.R
+++ plot3.R
@@ -19,7 +19,7 @@ subMetering_2 <- as.numeric(requiredData$Sub_metering_2)
 subMetering_3 <- as.numeric(requiredData$Sub_metering_3)
 
 ## Plot the diagram
-plot(datetime, subMetering_1, type="l", ylab="Energy Submetering", xlab="")
+plot(datetime, subMetering_1, type="l", ylab="Energy sub metering", xlab="")
 lines(datetime, subMetering_2, type="l", col="red")
 lines(datetime, subMetering_3, type="l", col="blue")
 legend("topright", c("Sub_metering_1", "Sub_metering_2", "Sub_metering_3"), lty=1, lwd=1, cex=1, pt.cex  = 1, col=c("black", "red", "blue"))
